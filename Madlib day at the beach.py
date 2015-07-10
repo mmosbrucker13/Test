e6bbcfb27ea7b9f98a373a469407415672dadd9c
mmosbrucker13 <kmlissakate13@gmail.com>
2015-07-09 22:28:44 -0500

    Added my Mad Lib file

---
+++ Madlib day at the beach.py
@@ -0,0 +1,10 @@
+print("There once was a famous icon " + "\nWho had to sleep with the light on.")
+input("Enter a female name.") 
+print("Whose name was " +input())
+input("Enter a verb.")
+print("She liked to " + input()+" While at the beach.")
+print("One day while soaking up the sun she got an idea.")
+input("Enter an idea.")
+print(input()+"was going to happen now.")
+print("She sprung up and raced off to persue her idea and the day became another wonderful day.") 
+
