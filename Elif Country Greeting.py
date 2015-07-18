0cc8e52814c954b464de023bb2f8b6b350f008e1
mmosbrucker13 <kmlissakate13@gmail.com>
2015-07-17 23:29:55 -0500

    Initial Commit

---
+++ Elif Country Greeting.py
@@ -0,0 +1,26 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Melissa
+#
+# Created:     17/07/2015
+# Copyright:   (c) Melissa 2015
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+country = input("Enter a country to recieve a greeting.").upper()
+if country == "CANADA":
+    print("Hello!")
+elif country == "FRANCE":
+    print("Bonjour")
+elif country == "SPAIN":
+    print("Hola!")
+elif country == "Germany":
+    print("Guten Tag")
+else:
+    ("Hello from America!")
+print("Nice to meet you!")
+
+
+
